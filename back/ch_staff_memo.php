6aefa3d67fe88a02e38475675f0c0d540594dc7c
rostyle <rostyle95@gmail.com>
2023-02-15 17:41:06 +0900

    new

---
+++ back/ch_staff_memo.php
@@ -0,0 +1,26 @@
+<?php
+    if(isset($_POST['user_id'])&&isset($_POST['memo'])){
+        include('../common.php');
+    
+        $user_id = $_POST['user_id'];
+        $memo = $_POST['memo'];
+    
+        try{
+            $db= new PDO($dsn,$user,$pass,[
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            ]);
+            $stmt = $db->prepare('UPDATE users SET memo=:memo WHERE user_id = :user_id');
+            $stmt->execute(array(':user_id' => $user_id,':memo' => $memo));
+        
+            $db = null;
+        }catch(PDOException $e){
+            echo '接続失敗' . $e->getMessage();
+            exit();
+        };
+        
+        header('Location: '.$_SERVER['HTTP_REFERER']);
+        exit();
+    }
+    
+    
+?>
