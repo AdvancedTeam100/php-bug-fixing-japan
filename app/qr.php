276a8529100a51c1cbc7ee97bf2db392e4bf4dc8
rostyle <rostyle95@gmail.com>
2023-02-16 22:03:36 +0900

    QRエラー修正

--- app/qr.php
+++ app/qr.php
@@ -1,15 +1,15 @@
+<?php require('header.php');?>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.qrcode/1.0/jquery.qrcode.min.js"></script> 
 <script type="text/JavaScript"> 
     $(function(){
-      var qrtext = "http://localhost/FeeHatCRM/back/qr_scan.php?user_id=".$user_data['id'];
+      var qrtext = "http://localhost/FeeHatCRM/back/qr_scan.php?user_id=<?=$user_id?>";
       var utf8qrtext = unescape(encodeURIComponent(qrtext));
       $("#img-qr").html("");
       $("#img-qr").qrcode({text:utf8qrtext}); 
     });
 </script>
-
-<?php require('header.php');?>
 
 <main>
     <h1>QRコード</h1>
