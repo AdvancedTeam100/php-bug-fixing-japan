912f2b825a339cff65fab81510a3dd204e7d2641
rostyle <rostyle95@gmail.com>
2023-03-15 13:03:50 +0900

    クーポン購入OK

--- app/point.php
+++ app/point.php
@@ -1,20 +1,37 @@
 <?php require('header.php');?>
 <?php
-    try{
-        $db= new PDO($dsn,$user,$pass,[
+    try {
+        $db = new PDO($dsn, $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ]);
+
+        // ユーザーの保有ポイントを取得
         $query = "SELECT SUM(point) FROM points WHERE user_id = :user_id";
         $stmt = $db->prepare($query);
         $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
         $stmt->execute();
         $points = $stmt->fetchColumn();
 
-    }catch(PDOException $e){
-          echo '接続失敗' . $e->getMessage();
-          exit();
+        // ユーザーが保有するクーポンを取得
+        $query = "SELECT * FROM coupons WHERE coupon_id IN (SELECT coupon_id FROM coupon_user WHERE user_id = :user_id)";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $coupons = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // 交換可能なクーポンを取得
+        $query = "SELECT * FROM coupons WHERE point_cost <= :points AND coupon_id NOT IN (SELECT coupon_id FROM coupon_user WHERE user_id = :user_id)";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':points', $points, PDO::PARAM_INT);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $exchangeableCoupons = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    } catch(PDOException $e) {
+        echo '接続失敗' . $e->getMessage();
+        exit();
     };
-    ?>
+?>
 <main>
     <div class="point">
         <h2>保有ポイント</h2>
@@ -23,34 +40,42 @@
     <br><br>
     <div class="Mycoupon">
         <h2>現在保有中のクーポン</h2>
-        <div class="Mycoupon_card">
-            <p>お会計10％PFFクーポン</p>
-            <form action="" method="post">
-                <input type="hidden" name="coupon" value="">
-                <button>使用する</button>
-            </form>
-        </div>
-        <div class="Mycoupon_card">
-            <p>お会計10％PFFクーポン</p>
-            <form action="" method="post">
-                <input type="hidden" name="coupon" value="">
-                <button>使用する</button>
-            </form>
-        </div>
+        <?php if (count($coupons) > 0): ?>
+            <?php foreach ($coupons as $coupon): ?>
+                <div class="Mycoupon_card">
+                    <p><?=$coupon['name']?></p>
+                    <form action="" method="post">
+                        <input type="hidden" name="coupon_id" value="<?=$coupon['coupon_id']?>">
+                        <button>使用する</button>
+                    </form>
+                </div>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <p>現在保有中のクーポンはありません。</p>
+        <?php endif; ?>
     </div>
     <br><br>
     <div class="coupon_list">
         <h2>保有ポイントでお得なクーポンをGET</h2>
-        <div class="coupon_card">
-            <p>お会計10％PFFクーポン</p>
-            お会計から10％お値引きできるクーポンです。
-            <form action="" method="post">
-                <input type="hidden" name="coupon" value="">
-                <button>ポイントを交換</button>
-            </form>
-        </div>
-    </div>
+        <?php if (count($exchangeableCoupons) > 0): ?>
+        <?php foreach ($exchangeableCoupons as $exchangeableCoupon): ?>
+            <div class="coupon_card">
+                <p><?=$exchangeableCoupon['name']?></p>
+                <?=$exchangeableCoupon['description']?>
+                <form action="back/buy_coupon.php" method="post" onsubmit="return confirm('本当に<?=$exchangeableCoupon['point_cost']?>ポイントを交換しますか？');">
+                    <input type="hidden" name="coupon_id" value="<?=$exchangeableCoupon['coupon_id']?>">
+                    <input type="hidden" name="point_cost" value="<?=$exchangeableCoupon['point_cost']?>">
+                    <input type="hidden" name="user_id" value="<?=$user_id?>">
+                    <button><?=$exchangeableCoupon['point_cost']?>ポイントを交換</button>
+                </form>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>現在保有中のクーポンはありません。</p>
+    <?php endif; ?>
+
+</div>
+<br><br>
 
 </main>
-
 <?php require('footer.php');?>
