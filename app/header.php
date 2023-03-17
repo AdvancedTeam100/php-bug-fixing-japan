02eed3ef250fa2ed29c277bd46bb548fcd4bfd61
rostyle <rostyle95@gmail.com>
2023-03-18 08:52:05 +0900

    ヘッダーリンク

--- app/header.php
+++ app/header.php
@@ -1,3 +1,22 @@
+<?php
+require('../common.php');
+$user_id = 1;
+
+try{
+    $db= new PDO($dsn,$user,$pass,[
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
+    $stmt = $db->prepare("SELECT * FROM users WHERE user_id=?");
+    $stmt->execute([$user_id]);
+    $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+}catch(PDOException $e){
+        echo '接続失敗' . $e->getMessage();
+        exit();
+};
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -9,5 +28,5 @@
 </head>
 <body>
 <header>
-    <h1>FeeHat App</h1>
+    <a href="./"><h1>FeeHat App</h1></a>
 </header>
