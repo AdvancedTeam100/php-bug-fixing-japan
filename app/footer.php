07153b8b8d826505465fee2f06b9a0e32d51ebc3
rostyle <rostyle95@gmail.com>
2023-03-25 18:21:02 +0900

    bottom-menu

--- app/footer.php
+++ app/footer.php
@@ -1,3 +1,11 @@
 <footer>
-    <p>&copy; FeeHat</p>
+  <nav class="bottom-menu">
+    <ul>
+      <li><a href="index.php"><i class="fas fa-home"></i><span>ホーム</span></a></li>
+      <li><a href="shop_info.php"><i class="fas fa-store"></i><span>店舗情報</span></a></li>
+      <li><a href="coupon_history.php"><i class="fas fa-ticket-alt"></i><span>クーポン</span></a></li>
+      <li><a href="mypage.php"><i class="fas fa-cog"></i><span>設定</span></a></li>
+    </ul>
+  </nav>
 </footer>
+
