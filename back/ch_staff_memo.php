af5a6c2cadd840a41b212a28a2b566c04b37c7ad
rostyle <rostyle95@gmail.com>
2023-04-20 00:43:57 +0900

    ダッシュボード作成

--- back/ch_staff_memo.php
+++ back/ch_staff_memo.php
@@ -4,13 +4,15 @@
     
         $user_id = $_POST['user_id'];
         $memo = $_POST['memo'];
-    
+        date_default_timezone_set('Asia/Tokyo');
+        $updated_at = date('Y-m-d H:i:s');
+        
         try{
             $db= new PDO($dsn,$user,$pass,[
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             ]);
-            $stmt = $db->prepare('UPDATE users SET memo=:memo WHERE user_id = :user_id');
-            $stmt->execute(array(':user_id' => $user_id,':memo' => $memo));
+            $stmt = $db->prepare('UPDATE users SET memo=:memo, updated_at=:updated_at WHERE user_id = :user_id');
+            $stmt->execute(array(':user_id' => $user_id,':memo' => $memo,':updated_at' => $updated_at));
         
             $db = null;
         }catch(PDOException $e){
---
+++ dashboard.php
@@ -0,0 +1,127 @@
+<?php
+require('common.php');
+
+try{
+    $db = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
+    $stmt = $db->prepare("SELECT * FROM users ORDER BY created_at DESC LIMIT 5");
+    $stmt->execute();
+    $created_at_user_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $stmt = $db->prepare("SELECT * FROM users ORDER BY updated_at DESC LIMIT 5");
+    $stmt->execute();
+    $updated_at_user_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}catch(PDOException $e){
+    echo '接続失敗' . $e->getMessage();
+    exit();
+};
+
+?>
+<?php require('header.php');?>
+    <?php require('menu.php');?>
+    <main>
+        <h1>dashboard</h1>
+        <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            チャート
+                        </div>
+                        <div class="card-body">
+                            <p>ここにチャートを表示します。</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            ランキング
+                        </div>
+                        <div class="card-body">
+                            <p>ここにランキングを表示します。</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            最近追加された会員
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>名前</th>
+                                        <th>作成日時</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($created_at_user_data as $u):?>
+                                        <tr>
+                                            <td><?php echo $u['user_id']; ?></td>
+                                            <td><?php echo $u['name']; ?></td>
+                                            <td><?php echo date('Y-m-d H:i', strtotime($u['created_at'])); ?></td>
+                                            <td>
+                                                <form action="user_info.php" method="get">
+                                                    <input type="hidden" name="user_id" value="<?=$u['user_id']?>">
+                                                    <button type="submit" class="btn btn-primary">詳細</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            最近更新されたメモ
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>名前</th>
+                                        <th>メモ</th>
+                                        <th>更新日時</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($updated_at_user_data as $u):?>
+                                        <tr>
+                                            <td><?php echo $u['user_id']; ?></td>
+                                            <td><?php echo $u['name']; ?></td>
+                                            <td><?php echo $u['memo']; ?></td>
+                                            <td><?php echo date('Y-m-d H:i', strtotime($u['updated_at'])); ?></td>
+                                            <td>
+                                                <form action="user_info.php" method="get">
+                                                    <input type="hidden" name="user_id" value="<?=$u['user_id']?>">
+                                                    <button type="submit" class="btn btn-primary">詳細</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+
+    </main>
+
+
+
+<?php require('footer.php');?>
