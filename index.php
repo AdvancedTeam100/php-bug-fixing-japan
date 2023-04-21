a20fe1493e386133f669ac349b1a8eb9871e1c38
rostyle <rostyle95@gmail.com>
2023-04-21 13:33:50 +0900

    require('dashboard.php')

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
-<?php require('header.php');?>
-    <?php require('menu.php');?>
-    <main>
-      <h1>ダッシュボード</h1>
-    </main>
-    <?php require('footer.php');?>
+<?php require('dashboard.php');?>
